89ce518a07fc145500ce6fe074a3cd00d52e3447
jlbuild <juliabuildbot@gmail.com>
2025-12-05 23:38:03 +0000

    coreutils_jll build 9.9.0+0

--- src/coreutils_jll.jl
+++ src/coreutils_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("coreutils")
-JLLWrappers.@generate_main_file("coreutils", UUID("5818bda4-868b-5068-b238-e370ed6eefef"))
+JLLWrappers.@generate_main_file("coreutils", Base.UUID("5818bda4-868b-5068-b238-e370ed6eefef"))
 end  # module coreutils_jll
